37b3c4f4aadaf6eff8be09b8986520e71b7d1b06
unknown <kehindekhourie@gmail.com>
2024-04-29 10:21:43 +0100

    minor changes to grid

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({gameQuery}: Props) => {
   const {data, errorMessage, isLoading} = useGames(gameQuery)
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-  if(error) return <Text>{error}</Text>
+  if(errorMessage) return <Text>{errorMessage}</Text>
 
   return (
     <SimpleGrid columns={{sm:1, md: 2, lg:3, xl:4}} spacing={6} padding={'10'}>
